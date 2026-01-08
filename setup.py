6960111d27a7e7971e85ecc71614d6fab48f9ed4
Gladys Godines <106888200+Gladys91@users.noreply.github.com>
2026-01-08 01:15:18 -0500

    Rename package from comp0188_cw2_2025 to comp0188_cw2_2526

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 HERE = pathlib.Path(__file__).parent
 
 setuptools.setup(
-    name='comp0188_cw2_2025',
+    name='comp0188_cw2_2526',
     version='1.0.0',
     description="",
     long_description="",
